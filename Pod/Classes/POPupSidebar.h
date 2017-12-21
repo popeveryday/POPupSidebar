ab288e28721592e528bb145007c57d791843c4d5
Trung Pham <p.hieu.trung@gmail.com>
2017-12-22 00:19:20 +0700

    0.1.43

--- Pod/Classes/POPupSidebar.h
+++ Pod/Classes/POPupSidebar.h
@@ -35,8 +35,9 @@ enum POPupSidebarProfileType
 @protocol POPupSidebarDelegate <NSObject>
 
 -(void) popUpDidSelectedItemWithKey:(NSString*)key currentViewController:(UIViewController*)view;
--(BOOL) popUpWillSelectedItemWithKey:(NSString*)key currentViewController:(UIViewController*)view;
 
+@optional
+-(BOOL) popUpWillSelectedItemWithKey:(NSString*)key currentViewController:(UIViewController*)view;
 -(void) popUpModifyCell:(UITableViewCell*)cell atIndexPath:(NSIndexPath*)indexPath key:(NSString*)key rowData:(Hashtable*)rowData;
 -(UITableViewCell*) popUpReturnCustomCellAtIndexPath:(NSIndexPath*)indexPath key:(NSString*)key rowData:(Hashtable*)rowData;
 
